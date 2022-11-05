66703f63d3688c06f1204acaaadb53f98edf4e06
Jurij Srebrnič <jurij.srebrnic@gmail.com>
2022-11-05 18:46:17 +0100

    Added helper functions to write to CSV

--- reddit_bot/items.py
+++ reddit_bot/items.py
@@ -8,9 +8,16 @@
 import scrapy
 
 class RedditEntryItem(scrapy.Item):
+    id = scrapy.Field()
     activity = scrapy.Field()
     date = scrapy.Field()
     url = scrapy.Field()
     title = scrapy.Field()
     body = scrapy.Field()
     subreddit = scrapy.Field()
+
+    def get_fields_names(self):
+        return ["id", "activity", "date", "url", "title", "body", "subreddit"]
+
+    def get_values(self):
+        return [self.get("id"), self.get("activity"), self.get("date"), self.get("url"), self.get("title"), self.get("body"), self.get("subreddit")]
